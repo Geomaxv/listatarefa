cc73f2ae8e97730ff3ee924c1ad500c8e8b228a2
GEOVANNY MAX VIEIRA DE SANTANA <2019311395@IFAL.LOCAL>
2022-10-27 08:35:18 -0300

    versão alpha 0.0.1

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import tarefaLista from './User.service';
 
 export default function App() {
+
+  const [filter, setFilter] = useState("");
+  const [tarefa, setTarefa] = useState([...tarefaLista]);
+  const [texto, setTexto] = useState();
+
+  /* useEffect(() => {
+    getUsers(filter).forEach((task)=>{setUsers(task)})
+    console.log(tarefa)
+  }, [filter]); */
+  const addTexto = () => {
+    tarefaLista.push({
+      name: texto, status: "pendente"
+    })
+    setTexto("")
+    setTarefa(tarefaLista)
+  }
+  const marcar = (index) => {
+    tarefaLista[index].status="concluido"
+    console.log(tarefaLista)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tarefas</Text>
+      <TextInput onChangeText={setTexto} style={styles.textInput}></TextInput>
+      <TouchableOpacity style={styles.botao} onPress={()=>addTexto()}><Text>Adicionar na lista</Text></TouchableOpacity>
+      {
+        tarefa.map((user, index) => (<View style={styles.tarefaContainer} key={index}>
+          <TouchableOpacity style={styles.check} onPress={()=>marcar(index)}></TouchableOpacity>
+          <Text key={user.name} style={styles.conteudo}>{user.name}</Text>
+          </View>
+        ))
+      }
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +49,35 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    border: "1px solid black",
+    margin: 10,
+  },
+  botao: {
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 3,
+  },
+  conteudo: {
+    fontSize: 15,
+
+
+  },
+  tarefaContainer: {
+    flexDirection: 'row',
+    display: "flex",
+    justifyContent: 'flex-start',
+    width: 125,
+    margin: 5
+
+  },
+  check: {
+    width: 10,
+    height: 10,
+    marginRight: 5,
+    borderRadius: 50,
+    borderColor: "black",
+    borderWidth: 1
+
+  }
 });
---
+++ User.service.js
@@ -0,0 +1,14 @@
+const tarefaLista = [
+    {
+        name: "Atividade PRMO",
+        status: "pendente",
+    },
+    {
+        name: "Terapia",
+        status: "pendente",
+    },
+];
+
+
+
+export default tarefaLista;
